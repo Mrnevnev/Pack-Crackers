bc628dc392de1e14bcb9565b771387ab886c3e0b
NevillAdeyeye <hiimnevill@gmail.com>
2024-07-24 22:41:39 -0700

    made game

--- Assets/# Scripts/TransitionManager.cs
+++ Assets/# Scripts/TransitionManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.UI;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Linq;
